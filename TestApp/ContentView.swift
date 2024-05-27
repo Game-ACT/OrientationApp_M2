ffe18d7898d95b8db28c0436c472326e59809500
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-05-27 13:50:57 +0700

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Thapat Auechaikasem on 27/5/67.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
